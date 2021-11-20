d320efa32c5248f9cf44c6481287a37ed239c618
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-20 23:09:40 +0200

    intial setup

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            string n1 = Console.ReadLine();
+            int nn1 = change(n1);
+            string n2 = Console.ReadLine();
+            int nn2= change(n2);
+            
+            Console.WriteLine(nn1 + nn2);
+            
+            
+
+
+            //Console.WriteLine("Hello World!"); 
+        }
+
+
+        public static int change(string valueAsString)
+        {
+            
+            int valueAsNumber;
+            bool isNumber = int.TryParse(valueAsString, out valueAsNumber);
+            return valueAsNumber;
+        }
+    }
+}
