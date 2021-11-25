e2b695d7626f2f035919e77d6ad36404fe3fd38c
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-25 14:46:40 +0200

    Functional Tema 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,27 +6,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array;
-            string n1 = Console.ReadLine();
-            int nn1 = change(n1);
-            string n2 = Console.ReadLine();
-            int nn2= change(n2);
-            
-            Console.WriteLine(nn1 + nn2);
-            
-            
+            Console.Write("Te tog sa introduci dimensiunea vetorului: ");
+            int dimensiuneVector = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int [dimensiuneVector];
 
+            Console.WriteLine("Introdu elementele vectorului: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
-            //Console.WriteLine("Hello World!"); 
-        }
+            Console.WriteLine("--------------------------------");
 
-
-        public static int change(string valueAsString)
-        {
-            
-            int valueAsNumber;
-            bool isNumber = int.TryParse(valueAsString, out valueAsNumber);
-            return valueAsNumber;
+            int sumaElemente = 0;
+            int mediaElemente = 0;
+            int contorPare = 0;
+            int minim = 0;
+            int maxim = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sumaElemente += array[i];
+                mediaElemente = sumaElemente / array.Length;
+                if(array[i] % 2 == 0)
+                { 
+                    contorPare++;
+                }
+                minim = array[0];
+                if (array[i] < minim)
+                {
+                    minim = array[i];
+                }
+                maxim = array[i];
+                if(array[i] > maxim)
+                {
+                    maxim = array[i];
+                }
+            }
+            Console.WriteLine("Suma este = " + sumaElemente);
+            Console.WriteLine("Media este = " + mediaElemente);
+            Console.WriteLine($"Sunt {contorPare} numere pare in vetorul dat.");
+            Console.WriteLine($"Minumul vectorului este = {minim}");
+            Console.WriteLine($"Maximul vectorului este = {maxim}");
         }
     }
 }
